8e385cf31a75986a3a3f550741b2a27ce8145cab
Tedi Mitiku <tedi.m52@gmail.com>
2024-08-02 15:45:06 -0400

    add plugin boilerplate

---
+++ main.py
@@ -0,0 +1,12 @@
+
+def create_flow(service_spec, deployment_spec, flow_uuid):
+    modified_deployment_spec = copy.deepcopy(deployment_spec)
+
+    return {
+        "deployment_spec": modified_deployment_spec,
+        "config_map": {
+        }
+    }
+	
+def delete_flow(config_map, flow_uuid):
+    return
